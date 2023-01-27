d01f6cfed752e7a96e73d8c078de7d93846ac65f
ayapes <web@ymok.me>
2023-01-27 15:57:56 +0900

    2023-01-27-02

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,37 +1,28 @@
 <template>
-    <h1>Hello World</h1>
-    <h3>本のタイトル</h3>
-    <ul v-for="datum in data" class="list-group">
-        <li  class="list-group-item">{{ datum.title }}</li>
-    </ul>
-<h3>ユーザー一覧</h3>
-    <ul v-for="user in users" class="list-group list-group-horizontal w-50">
-        <li class="list-group-item w-50 p-2">{{ user.name }}</li>
-        <li class="list-group-item w-50 p-2">{{ user.email }}</li>
-    </ul>
+    <h1>本の貸し出しデータベース</h1>
+<BookList/>
+<UserList/>
 </template>
 
 <script>
 import axios from "axios";
+import BookList from './components/BookList.vue';
+import UserList from './components/UserList.vue';
 
 export default {
     name: 'App',
+    components:{
+        BookList,
+        UserList
+    },
+
     data(){
       return {
-        data:'',
-        users:'',
+// 
       }
     },
     async mounted() {
-        const url_b = "http://127.0.0.1:8000/api/books";
-        const response_b = await axios.get(url_b);
-        console.log(response_b.data);
-        this.data = response_b.data;
-
-        const url_u = "http://127.0.0.1:8000/api/users";
-        const response_u = await axios.get(url_u);
-        console.log(response_u.data);
-        this.users = response_u.data;
+// 
     }
 }
 </script>
---
+++ resources/js/components/BookList.vue
@@ -0,0 +1,26 @@
+<template>
+    <h3>本のタイトル</h3>
+    <ul v-for="datum in data" class="list-group">
+        <li  class="list-group-item">{{ datum.title }}</li>
+    </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+
+export default {
+    name: 'Book',
+    data(){
+      return {
+        data:'',
+      }
+    },
+    async mounted() {
+        const url_b = "/api/books";
+        const response_b = await axios.get(url_b);
+        console.log(response_b.data);
+        this.data = response_b.data;
+    }
+}
+</script>
