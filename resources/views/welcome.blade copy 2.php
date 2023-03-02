622d9e2994573a3fd57ace3cee414501c4575dc9
ayapes <web@ymok.me>
2023-03-02 18:11:51 +0900

    2023-03-02

--- resources/views/welcome.blade copy 2.php
+++ resources/views/welcome.blade copy 2.php
@@ -8,6 +8,7 @@
         
 
         <title>Laravel Vite Vue</title>
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body>
